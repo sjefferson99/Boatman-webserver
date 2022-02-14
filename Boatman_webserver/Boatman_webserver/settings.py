7b3ea25ee6292673214e7e75b63eb1ea61bef027
Stephen Jefferson <stephen@sjefferson.co.uk>
2022-02-14 20:48:56 +0000

    Settings update

--- Boatman_webserver/Boatman_webserver/settings.py
+++ Boatman_webserver/Boatman_webserver/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-with open('secret_key.txt') as f:
+with open('Boatman_webserver/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
